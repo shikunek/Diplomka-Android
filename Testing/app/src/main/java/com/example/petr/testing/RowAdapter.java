97f79a650ab7aad361e38ad98e2ea47081316043
Petr Polansky <shikunek@gmail.com>
2017-10-16 12:11:45 +0200

    Uprava

--- Testing/app/src/main/java/com/example/petr/testing/RowAdapter.java
+++ Testing/app/src/main/java/com/example/petr/testing/RowAdapter.java
@@ -19,8 +19,6 @@ public class RowAdapter extends ArrayAdapter<String>
 {
     private final Context context;
     private final ArrayList<ProjectClass> project;
-    private ArrayList<String> mobileArray = new ArrayList<String>();
-    private final ArrayList<ProjectClass> project;
     private ArrayList<String> mobileArray = new ArrayList<>();
 
     public RowAdapter(Context context, ArrayList<ProjectClass> project , ArrayList<String> mobileArray) {
